e84981289e4e7bbd985817438c2f63f701d42b85
Nursel Demirkiran <demirkiran3@gmail.com>
2023-12-29 00:21:41 +0300

    Week14 React Forms added

---
+++ week14/src/store/configureStore.js
@@ -0,0 +1,10 @@
+
+
+import rootReducer from "./rootReducer";
+import { devToolsEnhancer } from "@redux-devtools/extension";
+import { legacy_createStore } from "redux";
+
+export function configureStore() {
+    return legacy_createStore(rootReducer, devToolsEnhancer())
+
+}
